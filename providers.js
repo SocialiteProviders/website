5ccda8ca4bedb0e2e259974c9386e9d612a2f7f2
Cameron Wilby <cameron@wilby.com>
2024-08-22 11:46:07 -0700

    Update providers.js

--- providers.js
+++ providers.js
@@ -711,6 +711,10 @@ module.exports = [
         slug: 'Xero', name: 'Xero',
         maintainers: [],
       },
+      {
+        slug: 'YNAB', name: 'You Need A Budget',
+        maintainers: ['cwilby']
+      }
     ],
   },
   {
